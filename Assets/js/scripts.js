85bc3dfc2a73cdedb7b6b5e6ba6356db7fad8e4e
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-19 15:21:09 -0700

    weather cards displays current temp when hike is clicked

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -5,7 +5,7 @@ const OWMkey = config.OWMkey;
 let lat = "47.608013";
 let lng = "-122.335167";
 
-// get lat lng from autocomplete input
+// get lat lng from city search
 function initialize() {
   var input = document.getElementById("searchCity");
   var autocomplete = new google.maps.places.Autocomplete(input);
@@ -19,7 +19,7 @@ function initialize() {
   });
 }
 
-// get 20 'best hikes' based on city search
+// get 20 'best hikes' nearby lat lng
 const getHikes = (lat, lng) => {
   const coord = new google.maps.LatLng(lat, lng);
 
@@ -36,22 +36,20 @@ const getHikes = (lat, lng) => {
   };
 
   const service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+  service.textSearch(request, listHikes);
 };
 
 // push first 5 'best hikes' results into Results sidebar
-function callback(results, status) {
+function listHikes(results, status) {
   console.log(results);
 
   const cards = document.getElementById("cards");
 
-  let name = results[i].name;
-
   for (var i = 0; i <= 5; i++) {
-    cards.innerHTML +=
-        `<div class="card block">
+    let name = results[i].name;
+    cards.innerHTML += `<div class="card block">
         <header class="card-header">
-            <p class="card-header-title" data-lon="" data-lng="">${name}</p>
+            <p class="card-header-title hikeBtn" data-lat="${lat}" data-lng="${lng}">${name}</p>
             <button class="card-header-icon" aria-label="more options">
             <span class="icon">
                 <i class="fas fa-angle-down" aria-hidden="true"></i>
@@ -60,11 +58,16 @@ function callback(results, status) {
         </header>
         </div>`;
   }
+
+  // set click listener to all cards created in loop above
+  document.querySelectorAll(".hikeBtn").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      getWeather(item.getAttribute("data-lat"), item.getAttribute("data-lng"));
+    });
+  });
 }
 
-// onclick hike > getWeather(lat, lng);
-
-// get weather based on coordinates from google autocomplete
+// get weather based on coordinates of selected hike
 const getWeather = (lat, lng) => {
   let queryURL =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
@@ -79,6 +82,8 @@ const getWeather = (lat, lng) => {
     .then((data) => {
       console.log(data);
 
+      let temp = document.getElementById("temp");
+      temp.innerHTML = data.current.temp;
     });
 };
 
