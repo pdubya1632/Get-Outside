3a2b188871ebdee174f127377cc75044bc095d91
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-20 21:25:24 -0700

    remove img from list card (temporarily)

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -78,11 +78,6 @@ function listHikes(results, status) {
               </span>
             </button>
           </header>
-          <div class="card-image">
-          <figure class="image is-4by3">
-            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-          </figure>
-        </div>
           <div class="card-content">
             <div class="content">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
