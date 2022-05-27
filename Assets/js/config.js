d4ab57e812b03ee23ea2bf165eb9c9dd395032c1
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-26 20:10:38 -0700

    revert back to my orig owm key

--- Assets/js/config.js
+++ Assets/js/config.js
@@ -2,5 +2,5 @@
 // set API Key
 const config = {
     GMkey : 'AIzaSyAaIWzZ6ZEr4bjNJQdgcOP5RU3yHDD9fpw',
-    OWMkey : '44f3ab2b81bfb6491f1e2ab2de69d9a3'
+    OWMkey : 'd085326e9285b26166dacc06e92c94ca'
 }
